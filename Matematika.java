98b625d656d09f9d1dba92ad5ed0701091939e1d
najwalatifah <144809119+najwalatifah@users.noreply.github.com>
2024-09-11 20:53:57 +0700

    Add files via upload

---
+++ Matematika.java
@@ -0,0 +1,15 @@
+package matematika;
+public class Matematika {
+    public int pertambahan(int a, int b) {
+        return a + b;
+    }    
+    public int pengurangan(int a, int b) {
+        return b - 5;
+    }
+    public int perkalian(int a, int b) {
+        return b * 3;
+    }
+    public int pembagian(int a, int b) {
+        return 21 / 2;
+    }
+}
