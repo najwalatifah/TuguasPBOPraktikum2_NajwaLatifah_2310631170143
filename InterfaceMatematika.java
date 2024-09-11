5278532a14b69f5c95ea42ca5d88cce18b1e877c
najwalatifah <144809119+najwalatifah@users.noreply.github.com>
2024-09-11 21:17:12 +0700

    Delete InterfaceMatematika.java

--- InterfaceMatematika.java
+++
@@ -1,8 +0,0 @@
-package matematika;
-public interface InterfaceMatematika {
-    int pertambahan(int a, int b);
-    int pengurangan(int a, int b);
-    int perkalian(int a, int b);
-    int pembagian(int a, int b);
-}
-
